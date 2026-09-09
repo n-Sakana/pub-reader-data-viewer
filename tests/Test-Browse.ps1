abb60d5434b75a7dfe60c2a7c5e33c7c3094299a
fin-berth <berth@fin>
2026-09-09 11:42:23 +0900

    fin-berth win-desktop: 2026-09-09 11:42:23

--- tests/Test-Browse.ps1
+++ tests/Test-Browse.ps1
@@ -27,9 +27,16 @@ $main=[System.Windows.Automation.AutomationElement]::FromHandle([IntPtr]$WindowH
 if ($main.Current.ProcessId -ne $TargetProcessId) { throw 'Window does not belong to the specified test process' }
 $root=[System.Windows.Automation.AutomationElement]::RootElement
 $trace=New-Object 'System.Collections.Generic.List[object]'
+# Every window of the process UIA can see: unowned ones are children of the
+# desktop, owned ones (the dialog surface, an owned picker) sit under their owner.
 function TopLevel {
     $cond=New-Object System.Windows.Automation.PropertyCondition([System.Windows.Automation.AutomationElement]::ProcessIdProperty,$TargetProcessId)
-    return @($root.FindAll([System.Windows.Automation.TreeScope]::Children,$cond))
+    $found=@($root.FindAll([System.Windows.Automation.TreeScope]::Children,$cond))
+    $windowType=New-Object System.Windows.Automation.PropertyCondition([System.Windows.Automation.AutomationElement]::ControlTypeProperty,[System.Windows.Automation.ControlType]::Window)
+    foreach ($w in $main.FindAll([System.Windows.Automation.TreeScope]::Descendants,$windowType)) {
+        if ($w.Current.NativeWindowHandle -ne 0 -and ($found | Where-Object { $_.Current.NativeWindowHandle -eq $w.Current.NativeWindowHandle }).Count -eq 0) { $found += $w }
+    }
+    return $found
 }
 function Node($scope,[string]$property,[string]$value) {
     $key=if ($property -eq 'id') {[System.Windows.Automation.AutomationElement]::AutomationIdProperty} else {[System.Windows.Automation.AutomationElement]::NameProperty}
